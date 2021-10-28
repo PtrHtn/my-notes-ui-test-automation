bb70bbe447c21c292f6395af7f68cae2c08a3858
PtrHtn <ptrhtn@gmail.com>
2021-10-28 19:18:04 -0300

    Add waitForVisibilityOf method

--- src/test/java/pages/BasePage.java
+++ src/test/java/pages/BasePage.java
@@ -1,11 +1,23 @@
 package pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import step_definitions.CucumberHooks;
 
 public class BasePage {
     private final WebDriver driver = CucumberHooks.driver;
     private final WebDriverWait driverWait = CucumberHooks.driverWait;
+
+    private void waitForVisibilityOf(By locator) {
+        try {
+            driverWait.until(ExpectedConditions.visibilityOfElementLocated(locator));
+        }catch (Exception exception) {
+            System.out.println("Something went wrong in waitForVisibilityOf method: " + exception);
+        }
+    }
+
+
 
 }
