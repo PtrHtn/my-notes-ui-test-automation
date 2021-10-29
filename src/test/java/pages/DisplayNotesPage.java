c7f74ca65c85bdffa437f4453f124c760b0abb5c
PtrHtn <ptrhtn@gmail.com>
2021-10-29 19:07:16 -0300

    Add noteTitleIsDisplayed method into DisplayNotesPage class

--- src/test/java/pages/DisplayNotesPage.java
+++ src/test/java/pages/DisplayNotesPage.java
@@ -1,5 +1,13 @@
 package pages;
 
+import org.openqa.selenium.By;
+
 public class DisplayNotesPage extends BasePage{
+
+    private By noteTitleLocator(String noteTitle){return By.xpath("//h2[text()='"+noteTitle+"']");}
+
+    public boolean noteTitleIsDisplayed(String noteTitle) {
+        return isDisplayed(noteTitleLocator(noteTitle));
+    }
 
 }
