a0098bdf247be9da56049db97e1adf0a1cdb7357
PtrHtn <ptrhtn@gmail.com>
2021-11-04 21:58:31 -0300

    Add buttonAddNoteLocator into HomePage class

--- src/test/java/pages/HomePage.java
+++ src/test/java/pages/HomePage.java
@@ -1,8 +1,13 @@
 package pages;
 
+import org.openqa.selenium.By;
+
 public class HomePage extends BasePage{
+
+    private final By buttonAddNoteLocator = By.linkText("Add a note");
 
     public void navigateTo(String url){
         goTo(url);
     }
+
 }
