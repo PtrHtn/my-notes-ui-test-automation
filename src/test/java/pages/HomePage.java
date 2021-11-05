e50656717b2b169903a6b05cafd48d92081a760a
PtrHtn <ptrhtn@gmail.com>
2021-11-04 21:59:01 -0300

    Add buttonPlusSignLocator into HomePage class

--- src/test/java/pages/HomePage.java
+++ src/test/java/pages/HomePage.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.By;
 public class HomePage extends BasePage{
 
     private final By buttonAddNoteLocator = By.linkText("Add a note");
+    private final By buttonPlusSignLocator = By.cssSelector("a[title='Add Note']");
 
     public void navigateTo(String url){
         goTo(url);
