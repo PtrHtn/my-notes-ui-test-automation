a01e7049467a62f9b27b86d12509f68f724efd4b
PtrHtn <ptrhtn@gmail.com>
2021-11-06 22:13:32 -0300

    Add noteContentLocator into DisplayNotesPage class

--- src/test/java/pages/DisplayNotesPage.java
+++ src/test/java/pages/DisplayNotesPage.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.By;
 public class DisplayNotesPage extends BasePage{
 
     private By noteTitleLocator(String noteTitle){return By.xpath("//h2[text()='"+noteTitle+"']");}
+    private By noteContentLocator(String noteContent){return By.xpath("//p[text()='"+noteContent+"']");}
 
     public boolean noteTitleIsDisplayed(String noteTitle) {
         return isDisplayed(noteTitleLocator(noteTitle));
