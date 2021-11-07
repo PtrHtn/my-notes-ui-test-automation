cccfd48ca23de943a6b926cb9cf63d8f86fcc36d
PtrHtn <ptrhtn@gmail.com>
2021-11-07 20:38:28 -0300

    Add heChoosesToCreateANewNoteByClickingOnTheAddANoteButton() into CreatingNote class

--- src/test/java/step_definitions/CreatingNote.java
+++ src/test/java/step_definitions/CreatingNote.java
@@ -1,6 +1,8 @@
 package step_definitions;
 
 import io.cucumber.java.en.Given;
+import io.cucumber.java.en.When;
+import pages.AddANotePage;
 import pages.HomePage;
 
 public class CreatingNote {
@@ -8,6 +10,12 @@ public class CreatingNote {
     @Given("John is on the homepage of My Notes site")
     public void johnIsOnTheHomepageOfMyNotesSite() {
         new HomePage().navigateTo("https://my-notes.now.sh/");
+    }
+
+    @When("he chooses to create a new note by clicking on the Add a note button")
+    public void heChoosesToCreateANewNoteByClickingOnTheAddANoteButton() {
+        new HomePage().pressAddANoteButton();
+        new AddANotePage().enterNoteTitle("New note").and().enterNoteContent("Note content").then().pressSaveButton();
     }
 
 }
