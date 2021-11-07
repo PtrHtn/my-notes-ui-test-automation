5f0811c373c07fd8e83f04736bbe61219067e883
PtrHtn <ptrhtn@gmail.com>
2021-11-06 22:15:08 -0300

    Add noteCardLocator into DisplayNotesPage class

--- src/test/java/pages/DisplayNotesPage.java
+++ src/test/java/pages/DisplayNotesPage.java
@@ -6,6 +6,7 @@ public class DisplayNotesPage extends BasePage{
 
     private By noteTitleLocator(String noteTitle){return By.xpath("//h2[text()='"+noteTitle+"']");}
     private By noteContentLocator(String noteContent){return By.xpath("//p[text()='"+noteContent+"']");}
+    private final By noteCardLocator = By.cssSelector("li[class='note-card']");
 
     public boolean noteTitleIsDisplayed(String noteTitle) {
         return isDisplayed(noteTitleLocator(noteTitle));
