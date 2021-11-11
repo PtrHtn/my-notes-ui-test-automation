f62f6555ec738e305488fea3e82d65b5f49439d3
PtrHtn <ptrhtn@gmail.com>
2021-11-10 21:40:22 -0300

    Add editButtonLocator into EditNotePage class

--- src/test/java/pages/EditNotePage.java
+++ src/test/java/pages/EditNotePage.java
@@ -1,4 +1,9 @@
 package pages;
 
+import org.openqa.selenium.By;
+
 public class EditNotePage extends BasePage{
+
+    private final By editButtonLocator = By.cssSelector("a[data-cy= 'edit-note']");
+
 }
