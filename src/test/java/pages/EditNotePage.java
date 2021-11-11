5e354159560f9bc8165bb266ea44f2c4fade917a
PtrHtn <ptrhtn@gmail.com>
2021-11-10 21:57:14 -0300

    Add pressEditButton() into EditNotePage class

--- src/test/java/pages/EditNotePage.java
+++ src/test/java/pages/EditNotePage.java
@@ -1,9 +1,14 @@
 package pages;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 
 public class EditNotePage extends BasePage{
 
     private final By editButtonLocator = By.cssSelector("a[data-cy= 'edit-note']");
 
+    public DisplayNotesPage pressEditButton() {
+        click(editButtonLocator);
+        return new DisplayNotesPage();
+    }
 }
