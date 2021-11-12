cc5779c2950208b5b0d72e73fd4415d3138c9e16
PtrHtn <ptrhtn@gmail.com>
2021-11-12 20:46:12 -0300

    Update johnCreatesANewNoteWithTheTitleAndTheContent method

--- src/test/java/step_definitions/EditingNote.java
+++ src/test/java/step_definitions/EditingNote.java
@@ -2,15 +2,19 @@ package step_definitions;
 
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import pages.AddANotePage;
+import pages.DisplayNotesPage;
 import pages.EditNotePage;
 import pages.HomePage;
+
+import static org.testng.Assert.assertTrue;
 
 public class EditingNote {
     @Given("John creates a new note with the title {string} and the content {string}")
     public void johnCreatesANewNoteWithTheTitleAndTheContent(String title, String content) {
-        new HomePage().pressAddANoteButton();
+        new HomePage().navigateTo("https://my-notes.now.sh/").and().pressAddANoteButton();
         new AddANotePage().enterNoteTitle(title).and().enterNoteContent(content).then().pressSaveButton();
     }
 
@@ -23,4 +27,5 @@ public class EditingNote {
     public void changesTitleOfTheNoteTo(String newTitle) {
         new AddANotePage().changeNoteTitle(newTitle);
     }
+
 }
