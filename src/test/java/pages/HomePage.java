4e2cdd19f6da5cfccb3be0c7aa08c36476918d0a
PtrHtn <ptrhtn@gmail.com>
2021-11-12 20:47:51 -0300

    Add and() into HomePage class

--- src/test/java/pages/HomePage.java
+++ src/test/java/pages/HomePage.java
@@ -7,6 +7,8 @@ public class HomePage extends BasePage{
     private final By buttonAddNoteLocator = By.linkText("Add a note");
     private final By buttonPlusSignLocator = By.cssSelector("a[title='Add Note']");
 
+    public HomePage and() { return this; }
+
     public HomePage navigateTo(String url){
         goTo(url);
         return this;
