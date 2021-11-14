cccb6ddb2d09af52c858f927447af0ddf3f9d6b7
PtrHtn <ptrhtn@gmail.com>
2021-11-14 20:07:20 -0300

    Add MyNotesRunnerTest class which is using TestNG instead JUnit

---
+++ src/test/java/test_runner/MyNotesRunnerTest.java
@@ -0,0 +1,35 @@
+package test_runner;
+
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+
+@CucumberOptions(
+        plugin = {"pretty"
+                , "json:target/cucumber-report/cucumber-default-json-report.json"
+                , "html:target/cucumber-report/cucumber-default-html-report.html"
+                , "summary"
+                , "me.jvt.cucumber.report.PrettyReports:target/cucumber-report"
+        },
+        features = {"classpath:feature_files"},
+        glue = {"classpath:step_definitions"},
+        monochrome = false,
+        dryRun = false,
+        tags = "@editing_note"
+//        tags = "@myNotes and @creating_note"
+//        tags = "@myNotes or @creating_note"
+//        tags = "not @smoke"
+
+/*
+@fast 	            Scenarios tagged with @fast
+@wip and not @slow 	Scenarios tagged with @wip that aren’t also tagged with @slow
+@smoke and @fast 	Scenarios tagged with both @smoke and @fast
+@gui or @database 	Scenarios tagged with either @gui or @database
+(@smoke or @ui) and (not @slow) For even more advanced tag expressions
+*/
+
+)
+
+public class MyNotesRunnerTest extends AbstractTestNGCucumberTests {
+}
+
