b8d4598baf0c6267520e46d4bf23446c9501ca96
PtrHtn <ptrhtn@gmail.com>
2021-11-15 19:38:20 -0300

    Add johnGotAnExistingNoteThatHeDoesNotNeedAnyMore() into DeletingNote class

--- src/test/java/step_definitions/DeletingNote.java
+++ src/test/java/step_definitions/DeletingNote.java
@@ -1,2 +1,13 @@
-package step_definitions;public class DeletingNote {
+package step_definitions;
+
+import io.cucumber.java.en.Given;
+import pages.AddANotePage;
+import pages.HomePage;
+
+public class DeletingNote {
+    @Given("John got an existing note that he does not need any more")
+    public void johnGotAnExistingNoteThatHeDoesNotNeedAnyMore() {
+        new HomePage().navigateTo("https://my-notes.now.sh/").and().pressAddANoteButton();
+        new AddANotePage().enterNoteTitle("New note").and().enterNoteContent("Note content").then().pressSaveButton();
+    }
 }
