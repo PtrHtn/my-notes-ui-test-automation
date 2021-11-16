7a8239a5dc62cdb38c962029b30d8084e5ea679f
PtrHtn <ptrhtn@gmail.com>
2021-11-16 20:50:15 -0300

    Add buttonDeleteLocator into HomePage class

--- src/test/java/pages/HomePage.java
+++ src/test/java/pages/HomePage.java
@@ -1,11 +1,13 @@
 package pages;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 
 public class HomePage extends BasePage{
 
     private final By buttonAddNoteLocator = By.linkText("Add a note");
     private final By buttonPlusSignLocator = By.cssSelector("a[title='Add Note']");
+    private final By buttonDeleteLocator = By.cssSelector("button[data-testid='delete-button']");
 
     public HomePage and() { return this; }
 
