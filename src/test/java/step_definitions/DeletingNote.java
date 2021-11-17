5134336c0b6c5baae45deb10a1fbe99017c9025a
PtrHtn <ptrhtn@gmail.com>
2021-11-17 20:49:32 -0300

    Add thisNoteShouldNotBeDisplayedAnyMore() into deleteNote class

--- src/test/java/step_definitions/DeletingNote.java
+++ src/test/java/step_definitions/DeletingNote.java
@@ -1,9 +1,13 @@
 package step_definitions;
 
 import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import pages.AddANotePage;
+import pages.DisplayNotesPage;
 import pages.HomePage;
+
+import static org.testng.Assert.assertTrue;
 
 public class DeletingNote {
     @Given("John got an existing note that he does not need any more")
@@ -15,5 +19,11 @@ public class DeletingNote {
     @When("he deletes this note")
     public void heDeletesThisNote() {
         new HomePage().pressXSignButton();
+    }
+
+    @Then("this note should not be displayed any more")
+    public void thisNoteShouldNotBeDisplayedAnyMore() {
+        assertTrue(new DisplayNotesPage().noNoteIsDisplayed());
+        assertTrue(new HomePage().addANoteButtonIsDisplayed());
     }
 }
