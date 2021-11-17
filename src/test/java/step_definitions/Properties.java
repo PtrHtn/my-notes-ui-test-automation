10cb98d16a11480f4fcbd6c737c3ed705462d2a3
PtrHtn <ptrhtn@gmail.com>
2021-11-17 20:47:18 -0300

    Add Properties class

---
+++ src/test/java/step_definitions/Properties.java
@@ -0,0 +1,9 @@
+package step_definitions;
+
+// final class can't be extended by other class
+public final class Properties {
+
+    // private constructor preventing class to be instantiated
+    private Properties(){}
+
+}
