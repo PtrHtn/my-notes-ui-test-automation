fbaeaa1c24f1b8fcb3a149ac710cf4c5448a5d97
PtrHtn <ptrhtn@gmail.com>
2021-11-18 19:20:00 -0300

    Add PROJECT_URL variable into Properties class

--- src/test/java/step_definitions/Properties.java
+++ src/test/java/step_definitions/Properties.java
@@ -6,4 +6,6 @@ public final class Properties {
     // private constructor preventing class to be instantiated
     private Properties(){}
 
+    public static final String  PROJECT_URL = "https://my-notes.now.sh/";
+
 }
